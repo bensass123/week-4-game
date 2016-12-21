2e73af741a8357df5e37dcf2c0ff0ff4de2b45a5
Ben Sass <bensass123@gmail.com>
2016-12-20 20:55:19 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,48 +5,23 @@ var enemies;
 var characters = [];
 var defender;
 
-//asetup restart further, fix defender.cap error
-
-function setupGame(){
-	console.log('restart');
-	attacksWaged = 0;
-	playerSelected = false;
-	characters =[];
-	enemies = [];
-	defender = null;
-	//adding character objects to array
-	characters.push(new Character(0, 'Luke', 150, 8, 10));
-	characters.push(new Character(0, 'Han', 160, 11, 12));
-	characters.push(new Character(0, 'Palpatine', 180, 15, 18));
-	characters.push(new Character(0, 'vader', 200, 12, 35));
+//clickfunctions
+function clickLuke(){
+		if (!playerSelected) {
+				$('#Luke').addClass('selected');
+				$('#Luke').removeClass('imgHolder');
+				player = characters[0];
+				setupEnemies(0);
+				playerSelected = true;
+				$("#yourChar").text("Your Character:");
+		}
+		else if (playerSelected && !defender) {
+			setDefender('Luke');
+			console.log('luke defender');
+			}
 }
 
-function Character(id, name, hp, ap, cap) {
-	this.id = id;
-	this.name = name;
-	this.hp = hp;
-	this.ap = ap;
-	this.cap = cap;
-}
-
-
-//Event listeners for Characters 
-$("#Luke").click(function() {
-	if (!playerSelected) {
-		$('#Luke').addClass('selected');
-		$('#Luke').removeClass('imgHolder');
-		player = characters[0];
-		setupEnemies(0);
-		playerSelected = true;
-		$("#yourChar").text("Your Character:");
-	}
-	else if (playerSelected && !defender) {
-		setDefender('Luke');
-		console.log('luke defender');
-	}
-});
-
-$("#Han").click(function() {
+function clickHan(){
 	if (!playerSelected) {
 		$('#Han').addClass('selected');
 		$('#Han').removeClass('imgHolder');
@@ -59,9 +34,9 @@ $("#Han").click(function() {
 		setDefender('Han');
 		console.log('Han');
 	}
-});
+}
 
-$("#Palpatine").click(function() {
+function clickPalpatine(){
 	if (!playerSelected) {
 		$('#Palpatine').addClass('selected');
 		$('#Palpatine').removeClass('imgHolder');
@@ -74,9 +49,9 @@ $("#Palpatine").click(function() {
 		setDefender('Palpatine');
 		console.log('Palpatine');
 	}
-});
+}
 
-$("#vader").click(function() {
+function clickVader(){
 	if (!playerSelected) {
 		$('#vader').addClass('selected');
 		$('#vader').removeClass('imgHolder');
@@ -88,12 +63,90 @@ $("#vader").click(function() {
 		setDefender('vader');
 		console.log('vader');
 	}
+}
+
+//asetup restart further, fix defender.cap error
+
+function setupGame(){
+	console.log('restart');
+	attacksWaged = 0;
+	playerSelected = false;
+	characters =[];
+	enemies = [];
+	defender = null;
+	//adding character objects to array
+	characters.push(new Character(0, 'Luke', 150, 20, 10));
+	characters.push(new Character(0, 'Han', 160, 11, 12));
+	characters.push(new Character(0, 'Palpatine', 180, 15, 18));
+	characters.push(new Character(0, 'vader', 200, 12, 35));
+}
+
+function resetDivs(){
+	$('#top').html('<div id="Luke" class="imgHolder" >		<h2><span class = "charName">Luke Skywalker</span></h2>	  	<img class="character" src="assets/images/oldLuke.png" alt="Luke">	  	<h2><span id = "LukeHP" class = "charHP">150</span></h2>	</div>	<div class = "imgHolder" id = "Han">		<h2><span class = "charName">Han Solo</span></h2>	  	<img class="character" src="assets/images/hanSolo.jpg" alt="Han Solo">	  	<h2><span id = "HanHP" class = "charHP">160</span></h2>	</div>	<div class = "imgHolder" id = "Palpatine">		<h2><span class = "charName" >Emperor Palpatine</span></h2>	  	<img class="character" src="assets/images/palpatine.png" alt="Palpatine">	  	<h2><span id = "PalpatineHP" class = "charHP">180</span></h2>	</div>	<div class = "imgHolder" id = "vader">		<h2><span class = "charName">Darth vader</span></h2>	  	<img class="character" src="assets/images/vader.jpg" alt="vader">	  	<h2><span id ="vaderHP" class = "charHP">200</span></h2>	</div>');
+	
+
+	//Luke.addEventListener('click', lukeClick)
+
+	$("#Luke").click(function() {
+	clickLuke();
+	});
+
+	$("#Han").click(function() {
+	clickHan();
+	});	
+
+	$("#Palpatine").click(function() {
+	clickPalpatine();
+	});
+
+	$("#vader").click(function() {
+	clickVader();
+	});		
+
+	setupGame();
+
+	$('#bottom').html("");
+	$('#defenderDiv').html("");
+	$('#attackInfo').html("");
+}
+
+function Character(id, name, hp, ap, cap) {
+	this.id = id;
+	this.name = name;
+	this.hp = hp;
+	this.ap = ap;
+	this.cap = cap;
+}
+
+
+
+
+$(document).ready(function() {
+
+
+//Event listeners for Characters 
+$("#Luke").click(function() {
+	clickLuke();
+	});
+
+
+$("#Han").click(function() {
+	clickHan();
+});
+
+$("#Palpatine").click(function() {
+	clickPalpatine();
+});
+
+$("#vader").click(function() {
+	clickVader();
 });
 
 //listener for buttons
 
 $('#restartBtn').click(function () {
-	setupGame();
+	resetDivs();
+	$('#restartBtn').addClass('invisible');
 });
 
 
@@ -111,6 +164,8 @@ $('#attackButton').click(function attack(){
 		updateStory(currentAttack, defender.cap);
 		console.log("attack");
 	}
+});
+
 });
 
 function lose() {
